45fd7da91b933821ac30b1db387c8ef5adb8dc88
Mr oNary <onary@onary.com>
2014-02-26 20:05:50 +0200

    Initialization

---
+++ wsgi/funny_img/funny_img/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for funny_img project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "funny_img.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
