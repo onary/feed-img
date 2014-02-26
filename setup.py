e5c60a84d794010c50dcb7c837fc24627fd5542c
Mr oNary <onary@onary.com>
2014-02-26 20:33:22 +0200

    repairing settings

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='img',
     version='1.0',
     description='OpenShift App',
-    author='Your Name',
+    author='S V',
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
     install_requires=['Django>=1.5',
